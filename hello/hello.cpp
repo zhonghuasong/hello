9b56ea61cce5f6c3638cededb0a3517cdb2c6284
钟华松 <694492007@qq.com>
2013-09-09 00:36:25 +0930

    hello

---
+++ hello/hello.cpp
@@ -0,0 +1,16 @@
+// hello.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cout<<"Hello World!"<<endl;
+	system("pause");
+	return 0;
+}
+
